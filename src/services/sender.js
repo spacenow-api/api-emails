844e6e846b1ca55aff8408e73d58d6192e0caa8c
Camila <kmi_218@hotmail.com>
2019-10-03 14:41:11 +1000

    remove camila email

--- src/services/sender.js
+++ src/services/sender.js
@@ -7,7 +7,7 @@ function generateEmailParams(template, data) {
     Source: 'no-reply@spacenow.com',
     Destination: {
       ToAddresses: [data.email],
-      BccAddresses: ['baydr@spacenow.com', 'barrett@spacenow.com', 'camila@spacenow.com']
+      BccAddresses: ['baydr@spacenow.com', 'barrett@spacenow.com']
     },
     ReplyToAddresses: ['no-reply@spacenow.com'],
     ConfigurationSetName: 'Emails',
@@ -22,7 +22,7 @@ exports.senderByTemplateData = async (templateName, emailDestination, templateDa
       Source: 'no-reply@spacenow.com',
       Destination: {
         ToAddresses: [emailDestination],
-        BccAddresses: ['baydr@spacenow.com', 'barrett@spacenow.com', 'camila@spacenow.com']
+        BccAddresses: ['baydr@spacenow.com', 'barrett@spacenow.com']
       },
       ReplyToAddresses: ['no-reply@spacenow.com'],
       ConfigurationSetName: 'Emails',
