d19f8e0557d7f9994b9018d5712842aafdba1149
Camila <kmi_218@hotmail.com>
2019-11-05 09:12:16 +1100

    instant host

--- src/services/booking.service.js
+++ src/services/booking.service.js
@@ -83,6 +83,9 @@ module.exports = {
     const locationObj = await Location.findOne({
       where: { id: listingObj.locationId }
     })
+    const listingData = await ListingData.findOne({
+      where: { listingId: listingObj.id }
+    })
     const checkIn = moment(bookingObj.checkIn)
       .tz('Australia/Sydney')
       .format('ddd, Do MMM, YYYY')
