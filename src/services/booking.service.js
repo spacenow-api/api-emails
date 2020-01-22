9084148a222fa958f589c32b56bccaab5fc73d91
Camila <kmi_218@hotmail.com>
2020-01-23 10:55:09 +1100

    test booking request emails

--- src/services/booking.service.js
+++ src/services/booking.service.js
@@ -429,7 +429,7 @@ module.exports = {
     let serviceFee = (bookingObj.basePrice * bookingObj.period - discountValue) * HOST_FEE
     let minimumTerm = listingData.minTerm ? listingData.minTerm : 1
     let term = 'day'
-    if (listing.bookingPeriod !== 'daily') term = listing.bookingPeriod.replace('ly', '')
+    if (listingObj.bookingPeriod !== 'daily') term = listingObj.bookingPeriod.replace('ly', '')
     if (minimumTerm > 1) term = term + 's'
 
     const hostMetadata = {
