843b614ea3a9365be8c0463e15b61c9c3cdd43c3
Camila Concha <camilaconcha@Camilas-iMac.local>
2020-01-22 14:03:49 +1100

    Complete listing

--- src/services/listing.service.js
+++ src/services/listing.service.js
@@ -47,17 +47,25 @@ module.exports = {
         })
         const coverPhoto = await listingCommons.getCoverPhotoPath(listing.id)
         const categoryAndSubObj = await listingCommons.getCategoryAndSubNames(listing.listSettingsParentId)
+        let minimumTerm = listingData.minTerm ? listingData.minTerm : 1
+        let term = 'day'
+        if (listing.bookingPeriod !== 'daily') term = listing.bookingPeriod.replace('ly', '')
+        if (minimumTerm > 1) term = term + 's'
         emailObj = {
           currentDate,
           appLink: process.env.NEW_LISTING_PROCESS_HOST,
           hostName: userProfile.firstName,
-          listTitle: listing.title,
+          hostPhoto: userProfile.picture,
+          listingTitle: listing.title,
           listingId: listing.id,
-          listImage: coverPhoto,
-          listAddress: `${location.address1}, ${location.city}`,
+          listingImage: coverPhoto,
+          listingAddress: `${location.address1}, ${location.city}`,
           basePrice: listingData.basePrice.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'),
           priceType: listing.bookingPeriod,
-          category: categoryAndSubObj.category
+          category: categoryAndSubObj.category,
+          capacity: listingData.personCapacity ? listingData.personCapacity : 1,
+          minimumTerm,
+          term
         }
         await senderService.senderByTemplateData('complete-listing-email', user.email, emailObj)
       }
@@ -71,7 +79,7 @@ module.exports = {
   /**
    * Send an email to when space is published.
    */
-  sendEmailPublishListing: async (id) => {
+  sendEmailPublishListing: async id => {
     try {
       let emailObj
       let currentDate = moment()
