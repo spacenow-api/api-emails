a2149cd6249ba4190d23cf31951ed55c205a4928
Camila <kmi_218@hotmail.com>
2020-01-31 13:01:44 +1100

    message

--- src/services/message.service.js
+++ src/services/message.service.js
@@ -129,11 +129,11 @@ module.exports = {
         try {
           const messageObj = await Message.findByPk(messageItem[0].messageId)
           if (messageObj.hostId === messageItem[0].sentBy) {
-            console.log('host', messageItem[0].id)
-            sendEmailNewMessageHost(messageItem[0].id)
-          } else {
-            console.log('guest', messageItem[0].id)
+            console.log('to guest', messageItem[0].id)
             sendEmailNewMessageGuest(messageItem[0].id)
+          } else {
+            console.log('to host ', messageItem[0].id)
+            sendEmailNewMessageHost(messageItem[0].id)
           }
         } catch (err) {
           return err
