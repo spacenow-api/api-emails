c52ce63d33b405e71836707d38d4dda7932ca7f4
Lucas <csalucasnascimento@gmail.com>
2020-01-31 12:36:34 +1100

    message function

--- src/services/message.service.js
+++ src/services/message.service.js
@@ -7,101 +7,95 @@ const Op = Sequelize.Op
 
 const { User, UserProfile, Message, MessageItem } = require('./../models')
 
+const sendEmailNewMessageHost = async messageItemId => {
+  try {
+    let emailObj
+    let currentDate = moment()
+      .tz('Australia/Sydney')
+      .format('dddd D MMMM, YYYY')
+      .toString()
+
+    const messageItemObj = await MessageItem.findOne({
+      where: {
+        id: messageItemId
+      }
+    })
+    const messageObj = await Message.findOne({
+      where: {
+        id: messageItemObj.messageId
+      }
+    })
+    const hostObj = await User.findOne({
+      where: { id: messageObj.hostId }
+    })
+    const hostProfileObj = await UserProfile.findOne({
+      where: { userId: messageObj.hostId }
+    })
+    const guestProfileObj = await UserProfile.findOne({
+      where: { userId: messageObj.guestId }
+    })
+
+    emailObj = {
+      currentDate,
+      appLink: process.env.NEW_LISTING_PROCESS_HOST,
+      messageId: messageObj.id,
+      hostName: hostProfileObj.firstName,
+      guestName: guestProfileObj.firstName,
+      guestPhoto: guestProfileObj.picture,
+      message: messageItemObj.content
+    }
+    await senderService.senderByTemplateData('message-host-email', hostObj.email, emailObj)
+  } catch (err) {
+    console.error(err)
+    return err
+  }
+}
+
+const sendEmailNewMessageGuest = async messageItemId => {
+  try {
+    let emailObj
+    let currentDate = moment()
+      .tz('Australia/Sydney')
+      .format('dddd D MMMM, YYYY')
+      .toString()
+
+    const messageItemObj = await MessageItem.findOne({
+      where: {
+        id: messageItemId
+      }
+    })
+    const messageObj = await Message.findOne({
+      where: {
+        id: messageItemObj.messageId
+      }
+    })
+    const guestObj = await User.findOne({
+      where: { id: messageObj.guestId }
+    })
+    const hostProfileObj = await UserProfile.findOne({
+      where: { userId: messageObj.hostId }
+    })
+    const guestProfileObj = await UserProfile.findOne({
+      where: { userId: messageObj.guestId }
+    })
+
+    emailObj = {
+      currentDate,
+      appLink: process.env.NEW_LISTING_PROCESS_HOST,
+      messageId: messageObj.id,
+      hostName: hostProfileObj.firstName,
+      guestName: guestProfileObj.firstName,
+      hostPhoto: guestProfileObj.picture,
+      message: messageItemObj.content
+    }
+    await senderService.senderByTemplateData('message-guest-email', guestObj.email, emailObj)
+  } catch (err) {
+    console.error(err)
+    return err
+  }
+}
+
 module.exports = {
-  /**
-   * Send an email to notify a message to a host user.
-   */
-  sendEmailNewMessageHost: async messageItemId => {
-    try {
-      let emailObj
-      let currentDate = moment()
-        .tz('Australia/Sydney')
-        .format('dddd D MMMM, YYYY')
-        .toString()
-
-      const messageItemObj = await MessageItem.findOne({
-        where: {
-          id: messageItemId
-        }
-      })
-      const messageObj = await Message.findOne({
-        where: {
-          id: messageItemObj.messageId
-        }
-      })
-      const hostObj = await User.findOne({
-        where: { id: messageObj.hostId }
-      })
-      const hostProfileObj = await UserProfile.findOne({
-        where: { userId: messageObj.hostId }
-      })
-      const guestProfileObj = await UserProfile.findOne({
-        where: { userId: messageObj.guestId }
-      })
-
-      emailObj = {
-        currentDate,
-        appLink: process.env.NEW_LISTING_PROCESS_HOST,
-        messageId: messageObj.id,
-        hostName: hostProfileObj.firstName,
-        guestName: guestProfileObj.firstName,
-        guestPhoto: guestProfileObj.picture,
-        message: messageItemObj.content
-      }
-      await senderService.senderByTemplateData('message-host-email', hostObj.email, emailObj)
-    } catch (err) {
-      console.error(err)
-      return err
-    }
-  },
-
-  /**
-   * Send an email to notify a message to a host user.
-   */
-  sendEmailNewMessageGuest: async messageItemId => {
-    try {
-      let emailObj
-      let currentDate = moment()
-        .tz('Australia/Sydney')
-        .format('dddd D MMMM, YYYY')
-        .toString()
-
-      const messageItemObj = await MessageItem.findOne({
-        where: {
-          id: messageItemId
-        }
-      })
-      const messageObj = await Message.findOne({
-        where: {
-          id: messageItemObj.messageId
-        }
-      })
-      const guestObj = await User.findOne({
-        where: { id: messageObj.guestId }
-      })
-      const hostProfileObj = await UserProfile.findOne({
-        where: { userId: messageObj.hostId }
-      })
-      const guestProfileObj = await UserProfile.findOne({
-        where: { userId: messageObj.guestId }
-      })
-
-      emailObj = {
-        currentDate,
-        appLink: process.env.NEW_LISTING_PROCESS_HOST,
-        messageId: messageObj.id,
-        hostName: hostProfileObj.firstName,
-        guestName: guestProfileObj.firstName,
-        hostPhoto: guestProfileObj.picture,
-        message: messageItemObj.content
-      }
-      await senderService.senderByTemplateData('message-guest-email', guestObj.email, emailObj)
-    } catch (err) {
-      console.error(err)
-      return err
-    }
-  },
-
   sendEmailMessageNotification: async () => {
     try {
       const pastHour = moment()
@@ -133,9 +127,9 @@ module.exports = {
         try {
           const messageObj = await Message.findByPk(messageItem[0].messageId)
           if (messageObj.hostId === messageItem[0].sentBy)
-            sendEmailNewMessageHost(item[0].id)
+            sendEmailNewMessageHost(messageItem[0].id)
           else
-            sendEmailNewMessageGuest(item[0].id)
+            sendEmailNewMessageGuest(messageItem[0].id)
         } catch (err) {
           return err
         }
