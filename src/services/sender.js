ebfd653e0b0197317077b1a387e078403eaef95f
Lucas Nascimento <csalucasnascimento@gmail.com>
2020-05-30 13:12:09 +1000

    Testing aws variables

--- src/services/sender.js
+++ src/services/sender.js
@@ -7,32 +7,37 @@ function generateEmailParams(template, destination, data) {
     Source: "no-reply@spacenow.com",
     Destination: {
       ToAddresses: [destination],
-      BccAddresses: process.env.BBC_EMAILS.split(" ")
+      BccAddresses: process.env.BBC_EMAILS.split(" "),
     },
     ConfigurationSetName: "Emails",
     Template: template,
-    TemplateData: JSON.stringify(data)
+    TemplateData: JSON.stringify(data),
   };
 }
 
 exports.senderByTemplateData = async (template, destination, data) => {
+  console.log("accessKeyId", process.env.AWS_ACCESS_KEY_ID);
+  console.log("secretAccessKey", process.env.AWS_SECRET_ACCESS_KEY);
+
   return ses
     .sendTemplatedEmail({
       Source: "no-reply@spacenow.com",
       Destination: {
         ToAddresses: [destination],
-        BccAddresses: process.env.BBC_EMAILS.split(" ")
+        BccAddresses: process.env.BBC_EMAILS.split(" "),
       },
       ConfigurationSetName: "Emails",
       Template: template,
-      TemplateData: JSON.stringify(data)
+      TemplateData: JSON.stringify(data),
     })
     .promise();
 };
 
-exports.sender = async event => {
+exports.sender = async (event) => {
+  console.log("accessKeyId", process.env.AWS_ACCESS_KEY_ID);
+  console.log("secretAccessKey", process.env.AWS_SECRET_ACCESS_KEY);
+
   const body = JSON.parse(event.body);
-  console.log("BODY ===>>>", body)
   const emailParams = generateEmailParams(body.template, body.destination, JSON.parse(body.data));
   return ses.sendTemplatedEmail(emailParams).promise();
 };
